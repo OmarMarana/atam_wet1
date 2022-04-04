bcef099d4cbf8eb8aa3428d097f9b58405e5758a
Omar Marana <omarmarana@campus.technion.ac.il>
2022-04-04 16:21:05 +0300

    ex2 not finished

--- ex2.asm
+++ ex2.asm
@@ -3,3 +3,18 @@
 .section .text
 _start:
 #your code here
+
+        movq $source, %rax
+        movq $destination, %rbx
+        movl num, %ecx
+        movl $0x0, %edx
+        cmp %ecx, %edx
+        jg NEG_HW1
+        jmp END_HW1
+
+    NEG_HW1:
+        xor $0xffffffff, %ecx
+        inc %ecx
+
+    END_HW1:
+        destination: .quad 0xdeadbeee
